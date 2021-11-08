e58989a30df72b2d08bce59140ba4d44205ffc46
kor <kor.dwarshuis@gmail.com>
2021-11-08 18:08:13 +0100

    New file

---
+++ localisation.example.js
@@ -0,0 +1,8 @@
+export const localisation = {
+    locale: "en",
+    textAppend1: "later",
+    textAppend2: "from the beginning",
+    configMenu: {
+        enterPathToCSV: "Enter the path to a csv file:"
+    }
+};
